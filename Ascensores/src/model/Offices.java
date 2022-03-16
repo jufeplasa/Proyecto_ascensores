195cd68dc144cb58e6a8fd66ab4183002d2ea890
Steban Rodriguez <amilcarshm@gmail.com>
2022-03-15 21:57:39 -0500

    class Offices

---
+++ Ascensores/src/model/Offices.java
@@ -0,0 +1,19 @@
+
+package model;
+
+public class Offices {
+    int numero;
+
+    public Offices(int numero) {
+        this.numero = numero;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    
+}
